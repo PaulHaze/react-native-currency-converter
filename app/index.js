0faeb2ccf603ab114a10b1a8766e63ea4393bed1
PaulHaze <20569604+PaulHaze@users.noreply.github.com>
2019-07-17 19:47:08 +1000

    folder reorganised

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Lets build a brandddd new currency converter!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
